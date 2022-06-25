27c88b07bc9648328acbb0c713ec356cadadfdf2
JayJajoo <84397446+JayJajoo@users.noreply.github.com>
2022-06-26 02:42:22 +0530

    added add/drop functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./app.css"
+import Items from "./Items";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
 
+  const [min, setMin] = useState(1);
+  const [sec, setSec] = useState(30);
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [info,setInfo]=useState([])
+
+  let aft="";
+  let block="block";
+
+  const submit=(event)=>{
+    event.preventDefault();
+    setInfo([...info,{name:name,email:email}])
+  }
+
+
+  useEffect(() => {
+    if (min >= 0) {
+      if (sec > 0) {
+        setTimeout(() => { setSec(sec - 1); }, 1000);
+      }
+      if (sec === 0 && min > 0) {
+        setMin(min - 1);
+        setSec(2);
+      }
+      if (min === 0 && sec === 0) {
+        setSec(0);
+        return;
+      }
+    }
+  }, [min, sec]);
+
+  const changes_email=(event)=>{
+    let eml=event.target.value;
+    if(eml.includes("@") && eml.includes(".com") && eml.indexOf("@")>0){
+      setEmail(eml);
+      document.getElementById("item").classList.add("aft");
+      document.getElementById("addexp").classList.remove("block");
+    }
+    else{
+      document.getElementById("item").classList.remove("aft");
+      document.getElementById("addexp").classList.add("block");
+    }
+  }
+
+  const changes_name=(event)=>{
+    setName(event.target.value)
+  }
+
+  return (
+    <div class="container">
+      <div class="clock">
+        <div class="flex  justify">
+          <div class="clock_box">{min}</div>
+          <div class="clock_box">{sec}</div>
+        </div>
+        <button class="start_timer" type="submit">Start</button>
+      </div>
+      <div class="add_drop">
+        <form onSubmit={submit}>
+          <div class="flex justify">
+          <input onChange={changes_name} class="date" type="text" placeholder="Name"></input>
+            <input onChange={changes_email} id="item" className={aft} type="text" placeholder="Email"></input>
+          </div>
+          <button id="addexp" className={block}>Add</button>
+        </form>
+      </div>
+      {info.length>0 && info.map((item)=>{return(<Items name={item.name} email={item.email}></Items>)})}
+    </div>
+  )
+}
 export default App;
---
+++ src/Items.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./app.css"
+
+function Items(props) {
+    const del_event=(event)=>{
+        document.getElementById("del_button").classList.add("del_effect");
+        document.getElementById("info").remove();
+    }
+
+  return (
+    <div id="info"className="flex">
+        <div class="image">
+            <img src='./bird.png'></img>
+        </div>
+        <div class="person_info">
+            <div class="person_name">{props.name}</div>
+            <div class="person_email">{props.email}</div>
+        </div>
+        <button onClick={del_event} className="" id="del_button"><img src="./delete.png"></img></button>
+    </div>
+  )
+}
+
+export default Items
